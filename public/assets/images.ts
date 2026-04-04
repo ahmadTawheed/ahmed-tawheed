21012eada4fd8b4fe71c98779a15bef5c42347fc
ahmadTawheed <ahmadtawheed74@gmail.com>
2026-04-04 21:25:35 +0200

    update profile web

--- public/assets/images.ts
+++ public/assets/images.ts
@@ -1,9 +1,10 @@
-export const images: { [key: string]: string } = {
-  web_1: "/assets/web_1.jpg",
-  web_2: "/assets/web_2.png",
-  web_3: "/assets/web_3.png",
-  app_1: "/assets/app_1.png",
-  app_2: "/assets/app_2.png",
-  app_3: "/assets/app_3.png",
-  dash_1: "/assets/dash_1.png",
-};
+export const images : { [key: string]: string } = {
+    web_1: "/assets/web_1.webp",
+    web_2: "/assets/web_2.webp",
+    web_3: "/assets/web_3.webp",
+    app_1: "/assets/app_1.webp",
+    app_2: "/assets/app_2.webp",
+    app_3: "/assets/app_3.webp",
+    dash_1: "/assets/dash_1.webp",
+
+}
